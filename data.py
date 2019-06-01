98b19249774bef1395a7c3c157a3ffedf23a56b4
Yihong Gu <wmyw96@126.com>
2019-06-01 18:53:36 +0800

    update dataset

--- data.py
+++ data.py
@@ -140,3 +140,77 @@ def sr_dataset(datadir, params, split_train=False):
         train_dataset = dataset(train_lr, train_hr)
 
     return train_dataset, valid_dataset, test_dataset
+
+
+def sr_ts_dataset(datadir, params, split_train=False):
+    # valid = train \belong test
+    train_set = set(params['data']['train'])
+    lr_img_size = (params['data']['size_w'], params['data']['size_h'])
+
+    train_lr = []
+    train_hr = []
+    valid_dataset = {}
+    test_dataset = {}
+    train_dataset = {}
+    for data_name in params['data']['test']:
+        path = os.path.join(datadir, data_name)
+        cur_list = os.listdir(path)
+        hr_img_cl = []
+        lr_img_cl = []
+        #print(len(cur_list))
+        pgb = progressbar.ProgressBar()
+        pgb.start()
+        count = 0
+
+        print('=' * 16 + '\nRead File: {}'.format(path))
+        for ele in cur_list:
+            if ele[0] == '.':
+                continue
+            count += 1
+            pgb.update(count)
+
+            file_path = os.path.join(path, ele)
+
+            if os.path.isfile(file_path):
+                img = io.imread(file_path)
+                hr_img_cl.append(np.expand_dims(img, axis=0))
+                #print(img.max())
+                im = Image.open(file_path)
+                shrink = im.resize(lr_img_size, Image.ANTIALIAS)
+
+                lr_img_cl.append(np.expand_dims(shrink, axis=0))
+        pgb.finish()
+        
+        lr_img_dat = np.concatenate(lr_img_cl, axis=0)
+        hr_img_dat = np.concatenate(hr_img_cl, axis=0)
+        n_data = lr_img_dat.shape[0]
+        print('Number of data = {}, [{}, {}]'.format(n_data, hr_img_dat.shape[1], hr_img_dat.shape[2]))
+        if data_name in train_set:
+            print('{}: TRAIN'.format(data_name))
+            if split_train:
+                train_dataset[data_name] = \
+                    dataset(lr_img_dat[:n_data // 10 * 6, :],
+                            hr_img_dat[:n_data // 10 * 6, :])
+            else:
+                print('append')
+                train_lr.append(lr_img_dat[:n_data // 10 * 6, :])
+                train_hr.append(hr_img_dat[:n_data // 10 * 6, :])
+
+            valid_dataset[data_name] = \
+                dataset(lr_img_dat[n_data // 10 * 6:n_data // 10 * 8, :],
+                        hr_img_dat[n_data // 10 * 6:n_data // 10 * 8, :])
+            test_dataset[data_name] = \
+                dataset(lr_img_dat[n_data // 10 * 8:, :],
+                        hr_img_dat[n_data // 10 * 8:, :])
+        else:
+            test_dataset[data_name] = \
+                dataset(lr_img_dat, hr_img_dat)
+
+    if not split_train:
+        train_lr = np.concatenate(train_lr, axis=0)
+        train_hr = np.concatenate(train_hr, axis=0)
+        train_dataset = dataset(train_lr, train_hr)
+
+    return train_dataset, valid_dataset, test_dataset
+
+
